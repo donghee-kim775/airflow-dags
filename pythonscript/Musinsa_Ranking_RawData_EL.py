216fb19298326d769bf3e60f79ce519439055da8
DongHee Kim <54052704+donghee-kim775@users.noreply.github.com>
2025-01-02 02:38:31 +0900

    Update Musinsa_Ranking_RawData_EL.py

--- pythonscript/Musinsa_Ranking_RawData_EL.py
+++ pythonscript/Musinsa_Ranking_RawData_EL.py
@@ -13,8 +13,8 @@ import sys
 import shutil
 
 # s3 connect
-aws_access_key_id='',
-aws_secret_access_key='',
+aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 region_name='ap-northeast-2'
 
 s3_client = connect_s3(aws_access_key_id, aws_secret_access_key, region_name)
