bd338c9f7875bb652a2b67bbe1a756c55d6ad22c
DongHee Kim <54052704+donghee-kim775@users.noreply.github.com>
2025-01-03 00:30:21 +0900

    Update musinsa_ranking_rawdata_el_dag.py

--- musinsa_ranking_rawdata_el_dag.py
+++ musinsa_ranking_rawdata_el_dag.py
@@ -1,72 +1,81 @@
-import boto3
-import logging
-import requests
-import os
-import json
-from s3_validate import connect_s3, validate_and_upload_s3_file
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.operators.dummy import DummyOperator
 
-from datetime import datetime, timedelta
+from airflow.utils.dates import days_ago
+from datetime import timedelta
+from kubernetes.client import models as k8s
 
-import time, random
-
-import sys
-import shutil
-
-import argparse
-
-# s3 connect
-aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-region_name='ap-northeast-2'
-
-s3_client = connect_s3(aws_access_key_id, aws_secret_access_key, region_name)
-
-# url
-url = "https://api.musinsa.com/api2/hm/v2/pans/ranking/sections/199?"
-
-# headers
-headers = {
-    "accept": "application/json, text/plain, */*",
-    "accept-encoding": "gzip, deflate, br, zstd",
-    "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-    "origin": "https://www.musinsa.com",
-    "referer": "https://www.musinsa.com/",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+# DAG 기본 설정
+default_args = {
+    'owner': 'ehdgml7755@cu.ac.kr',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 24,
+    'retry_delay': timedelta(minutes=30),
 }
 
-# params
-params = {
-    "storeCode" : "musinsa",
-    "period" : "DAILY",
+SEXUAL_DYNAMIC_PARAMS = [{
+    "SEXUAL" : "F"
+}, {
+    "SEXUAL" : "M"
+}, {
+    "SEXUAL" : "A"
 }
+]
 
-# categoryCode
-categoryCode = ["103000", "002000", "001000"] # 카테고리가 늘어남에 따라 수정 필요
+AGE_BAND_DYNAMIC_PARAMS = [{
+    "AGE_BAND" : "AGE_BAND_ALL"
+}, {
+    "AGE_BAND" : "AGE_BAND_MINOR"
+}, {
+    "AGE_BAND" : "AGE_BAND_20"
+}, {
+    "AGE_BAND" : "AGE_BAND_25"
+}, {
+    "AGE_BAND" : "AGE_BAND_30"
+}, {
+    "AGE_BAND" : "AGE_BAND_35"
+}, {
+    "AGE_BAND" : "AGE_BAND_40"
+}
+]
 
-# today_date
-today_date = datetime.now().strftime("%Y-%m-%d")
+# DAG 정의
+with DAG(
+    dag_id='Musinsa_Ranking_RawData_EL_DAG',
+    default_args=default_args,
+    description='musinsa ranking raw data extraction and loading to s3',
+    schedule_interval=None,
+    start_date=days_ago(1),
+    catchup=False,
+    tags=['musinsa', 'ranking_rawdata', 'Extract', 'Load', 'S3', 'k8s']
+) as dag:
 
-def main():
-    parser = argparse.ArgumentParser(description="SEXUAL / AGEBAND")
+    # Start task
+    start = DummyOperator(
+                task_id="start"
+            )
     
-    parser.add_argument('gf', type=str, help='parameter : SEXUAL')
-    parser.add_argument('ageBand', type=str, help='parameter : age_band')
-    
-    args = parser.parse_args()
-    
-    params['gf'] =  args.gf
-    params['ageBand'] = args.agBand
-    
-    for code in categoryCode:
-        response = requests.get(url, headers=headers, params=params)
-        if response.status_code == 200:
-            response_json = response.json()
-            logging.info("response 200")
+    for i, sexual_dct in enumerate(SEXUAL_DYNAMIC_PARAMS):
+        sexual_task = DummyOperator(
+                        task_id=f'{sexual_dct["SEXUAL"]}_task'
+        )
         
-        bucket_name = 'project4-raw-data'
-        file_name = f"{today_date}/Musinsa/RankingData/{code}/musinsa_{params['gf']}_{params['ageBand']}_{code}.json"
-
-        validate_and_upload_s3_file(s3_client, bucket_name, file_name, response_json)
-    
-if __name__ == "__main__":
-    main()
+        start >> sexual_task
+        
+        for j, age_band_dct in enumerate(AGE_BAND_DYNAMIC_PARAMS):
+            ageband_task = KubernetesPodOperator(
+                                task_id=f'{sexual_dct["SEXUAL"]}_{age_band_dct["AGE_BAND"]}_task',
+                                name=f'{sexual_dct["SEXUAL"]}_{age_band_dct["AGE_BAND"]}_task',
+                                namespace='airflow',
+                                image='ehdgml7755/project4-custom:latest',
+                                cmds=['python', './pythonscript/musinsa_ranking_rawdata_el.py'],
+                                arguments=[sexual_dct["SEXUAL"], age_band_dct["AGE_BAND"]],
+                                is_delete_operator_pod=True,
+                                get_logs=True,
+            )
+            
+            sexual_task >> ageband_task
