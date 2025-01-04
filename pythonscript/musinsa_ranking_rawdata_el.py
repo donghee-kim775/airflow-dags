0f314379bebbf6d6de5a8c53ef234dfb917db7e3
DongHee Kim <54052704+donghee-kim775@users.noreply.github.com>
2025-01-05 03:07:45 +0900

    commit

--- pythonscript/musinsa_ranking_rawdata_el.py
+++ pythonscript/musinsa_ranking_rawdata_el.py
@@ -44,35 +44,49 @@ params = {
 # today_date
 today_date = datetime.now().strftime("%Y-%m-%d")
 
+# mapping table - 2depth 카테고리를 이용해 airflow에서 task를 분기 중
+# task 명은 영어로 만들어야 함. 하지만 파일 적재는 한국어로 통일해서 적재 계획 (4depth가 한국어)
+# => 2depth 카테고리를 한국어로 바꿔야 함
+def mapping_2depth_kor(depth2category):
+    if depth2category == 'top':
+        return '상의'
+    elif depth2category == 'bottom':
+        return '하의'
+    elif depth2category == 'shoes':
+        return '신발'
+    elif depth2category == 'outer':
+        return '아우터'
+    
 def main():
     parser = argparse.ArgumentParser(description="sexual/category param")
     parser.add_argument('sexual', type=str, help='sexual')
     parser.add_argument('category_data', type=str, help='category')
 
-    # 인자 파싱
     args = parser.parse_args()
 
-    # 전달된 JSON 데이터를 파싱
-    sexual_data = json.loads(args.sexual) 
+    sexual_data = json.loads(args.sexual)
     category_data = json.loads(args.category_data)
 
-    # 파싱된 데이터 사용
-    print(f"Sexual data: {sexual_data}")
-    print(f"Category data: {category_data}")
-    """
-    logging.info(f"Category : {category} Crawler Start")
-    for code in category_codes:
-        params['categoryCode']=code
-        response = requests.get(url, headers=headers, params=params)
-        if response.status_code == 200:
-            response_json = response.json()
-            logging.info("response 200")
-        
-        bucket_name = 'project4-raw-data'
-        file_name = f"{today_date}/Musinsa/RankingData/{code}/musinsa_{params['gf']}_{code}.json"
+    params['gf'] = sexual_data[0]
 
-        validate_and_upload_s3_file(s3_client, bucket_name, file_name, response_json)
-     """   
+    logging.info(f"Category : {sexual_data[0]}_{category_data[0]} Crawler Start")
+   
+    for category_info in category_data[1]:
+        category3depth = list(category_info.items())[0]
+        
+        for category4code, category4name in category3depth[1].items():
+            params['categoryCode'] = category4code
+            response = requests.get(url, headers=headers, params=params)
+            
+            if response.status_code == 200:
+                response_json = response.json()
+                logging.info("response 200")
+            
+            bucket_name = 'project4-raw-data'
+            file_name = f"{today_date}/Musinsa/RankingData/{category3depth[0]}/{sexual_data[1]}_{sexual_data[1]}_{category3depth[0]}_{category4name}.json"
+    
+            validate_and_upload_s3_file(s3_client, bucket_name, file_name, response_json)
+  
 if __name__ == "__main__":
     main()
     
